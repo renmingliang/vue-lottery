b68957d136c9f95cfa9137447b033f1cb49e63c7
renmingliang <renmingliang0736@qq.com>
2018-01-26 16:29:03 +0800

    edit award

--- src/mock/lotteryAPI.js
+++ src/mock/lotteryAPI.js
@@ -5,13 +5,14 @@ const temp = {
   add: {errorInfo: '成功', result: '1', data: ''}
 }
 
+// 模拟用户数据
 const userData = []
-const userCount = 100
-for (let i = 0; i < userCount; i++) {
+const userCount = 1000
+for (let i = 1; i < userCount; i++) {
   userData.push(Mock.mock({
     Company: `公司测${i}`,
     CompleteID: '@increment',
-    HeadImg: `http://test.baoxianadmin.com/static/m/images/headImage/${i + 1}.jpg`,
+    HeadImg: `http://test.baoxianadmin.com/static/m/images/headImage/${i}.jpg`,
     Name: `测${i}`,
     Num: `M${i}`,
     OpenID: '@increment',
@@ -19,21 +20,24 @@ for (let i = 0; i < userCount; i++) {
   }))
 }
 
-const type = []
-const typeCount = 5
-for (let i = 0; i < typeCount; i++) {
-  type.push(Mock.mock({
-    value: '@increment',
-    label: '特等奖',
-    number: '13'
-  }))
-}
+const type = [
+  {value: '9', label: '特等奖', number: '3'},
+  {value: '1', label: '一等奖', number: '5'},
+  {value: '2', label: '二等奖', number: '12'},
+  {value: '3', label: '三等奖', number: '20'},
+  {value: '4', label: '参与奖', number: '28'},
+  {value: '5', label: '其他', number: ''}
+]
 
 export default {
   postLogin: config => {
     // console.log(config.body)
     const params = config.body.split('=')[1]
-    return temp[params]
+    if (temp[params]) {
+      return temp[params]
+    } else {
+      return {errorInfo: '密码错误', result: '0', data: ''}
+    }
   },
   checkLogin: config => {
     return temp['check']
@@ -45,7 +49,6 @@ export default {
     }
   },
   postDatas: config => {
-    console.log(config.body)
     return temp['add']
   }
 }
